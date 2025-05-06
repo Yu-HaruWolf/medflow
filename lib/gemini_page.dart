93a1effea3cd657700242fda3640adba32b289f0
SeiKdesu <g2581429@tcu.ac.jp>
2025-05-06 11:15:00 +0900

    change url

--- lib/gemini_page.dart
+++ lib/gemini_page.dart
@@ -605,8 +605,8 @@ ECG：異常なし
   // GETリクエストを送信する関数
   Future<Map<String, dynamic>?> fetchWeatherData(String prompt) async {
     final Uri url = Uri.parse(
-      'https://asia-northeast1-solution-challenge-458913.cloudfunctions.net/python-http-function?$prompt',
-    );
+      'https://asia-northeast1-solution-challenge-458913.cloudfunctions.net/python-http-function',
+    ).replace(queryParameters: {'prompt': prompt});
 
     try {
       final response = await http.get(url);
