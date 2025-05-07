e95d6acf95ff71861c65fd946199bff1158f507f
Yuki <yuki@haruwolf.dev>
2025-05-08 05:22:41 +0900

    remove mock data

--- lib/data/patient_repository.dart
+++ lib/data/patient_repository.dart
@@ -4,42 +4,7 @@ import 'package:solution_challenge_tcu_2025/data/nursing_plan.dart';
 import 'patient.dart';
 
 class PatientRepository {
-  List<Patient> patientList = List<Patient>.empty(growable: true);
   final db = FirebaseFirestore.instance.collection('patients');
-  PatientRepository() {
-    patientList.add(
-      Patient(
-        personalInfo: PersonalInfo(
-          furigana: 'さんぷる',
-          name: '怪盗キッド',
-          birthday: DateTime(2000, 1, 1),
-          address: 'Japan',
-          tel: '00000000',
-        ),
-        nursingPlan: NursingPlan(
-          nanda_i: "自己健康管理促進準備状態",
-          goal: "退院後は一人暮らしを自身で行えるようにする",
-          op: """創部の状態
-                床病の有無
-                バイタル
-                松葉杖の使用状態
-                入院中の日常生活動作
-                退院後、介助してくれる人がいるのか。
-                仕事の状況
-                可動域の有無
-                食事の状況""",
-          tp: """痛みの強さの時間を確認し、緩和策を助言
-退院後、電車で出社し駅から遠いため、理学療法士と協力し、リハビリ以外でも積極的に松葉杖の使用を促したり、リハビリを行ってもらう
-退院後、一人暮らしのため、日常生活は自身でできるように関わる。また、家では布団で寝ついているため、自身でできないのであれば、福利用具の導入を提案する。
-糖尿病があるため、栄養士と連携して食事指導を行う。""",
-          ep: """痛みは夜に強くなるようなので、食後ではなく、９時１５時、２１時で飲むようにつたえる　
-出社するために早めに家を出るなど工夫してもらうように伝える
-リハビリを行う。（具体的なメニューを書く）また、一人で起き上がれないため、初めて使用する用具の使い方を指導する。さらに、雨の日など傘がさせないため、カッパを使用してもらったする
-糖尿病があると、創の回復が遅くなったり、感染など合併症になるリスクも指導して食事の指導をする。""",
-        ),
-      ),
-    );
-  }
 
   Future<Patient?> getPatient(String id) async {
     final querySnapshot = await db.doc(id).get();
@@ -54,7 +19,7 @@ class PatientRepository {
   }
 
   Future<List<Patient>> get getAllPatients async {
-    patientList = <Patient>[];
+    final patientList = <Patient>[];
     final querySnapshot = await db.get();
     for (var doc in querySnapshot.docs) {
       patientList.add(Patient.fromJson(doc.data()));
